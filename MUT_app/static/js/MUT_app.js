78a49e812f3261f43793f50e865af92584ed827d
avamay <avamay481516@gmail.com>
2021-08-08 15:20:22 +0300

    part 2.

--- MUT_app/static/js/MUT_app.js
+++ MUT_app/static/js/MUT_app.js
@@ -1,3 +1,8 @@
 $('.sidebar a[href="' + window.location.pathname + '"]').addClass('active');
 
-
+/*
+$('#btn_header').click(function() {
+    console.log('work');
+        document.getElementById("container_0").style.marginLeft = "200px";
+    });
+*/
